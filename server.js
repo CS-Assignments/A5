5e0ca800aa85f638d0f22a59edb6a29fb4fa6bfa
Jackson <jackson@Jacksons-MBP.hsd1.or.comcast.net>
2020-06-02 19:50:28 -0700

    Completed Login

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var userData = require('./userData');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -16,10 +17,23 @@ app.get('/landing.html', function (req, res, next) {
     res.status(200).sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
-app.get('/mainShop/:num', function (req, res, next) {
-    userNum = req.params.num-1;
-    console.log("userNum", userNum);
-    res.status(200).sendFile(path.join(__dirname, 'public', 'mainShop.html'));
+app.get('/checking/:credentials', function (req, res, next) {
+    userNum = -1;
+    var creds = req.params.credentials;
+    var userInput = creds.split("_");
+    for(i = 0; i < userData.length; i++) {
+        if (userInput[0] == userData[i][0] && userInput[1] == userData[i][1]) {
+            userNum = i;
+            console.log("index", i);
+        }
+    }
+    
+    if (userNum == -1) {
+        console.log("invalid username or password");
+	res.status(200).sendFile(path.join(__dirname, 'public', 'landing.html'))
+    } else {
+	res.status(200).sendFile(path.join(__dirname, 'public', 'mainShop.html'));
+    }
 });
 
 app.get('*', function (req, res) {

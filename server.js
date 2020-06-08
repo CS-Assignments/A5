ffdeb9487bd8c1c9df8a056014877884b966e708
Jackson <jackson@Jacksons-MBP.hsd1.or.comcast.net>
2020-06-07 19:41:39 -0700

    Added likes and randomly generated home page

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
 var userData = require('./userData');
-var clothesData = require('./clothes')
+var clothesData = require('./clothes');
+var stickersData = require('./stickers');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+//since the landing page didn't have the navbar it wasn't included as the main template
 app.engine('handlebars', exphbs({ defaultLayout: null}));
 app.set('view engine', 'handlebars');
 
-var curUser = "";
+//if the user doesn't sign in they will automatically be assumed to be a guest
+var curUser = "guest";
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -27,15 +30,34 @@ app.get('/landing.html', function (req, res, next) {
 
 app.get('/mainShop.html', function(req, res, next) {
     console.log("enter main shop go");
-    if (curUser == "") {
-        curUser = "guest";
+    var randomArr = [];
+
+    var randomSticker = Math.floor(Math.random() * (stickersData.length-1));
+    var randomClothe = Math.floor(Math.random() * (clothesData.length-1));
+
+    //This block of if else statements essentially picks one random elements from each category
+    //along with the item before or after it depending on which random number was generated
+    if (randomSticker != (stickersData.length-1)) {
+        randomArr.push(stickersData[randomSticker]);
+        randomArr.push(stickersData[randomSticker+1]);
+    } else {
+        randomArr.push(stickersData[randomSticker]);
+        randomArr.push(stickersData[randomSticker-1]);
     }
-    res.render('shopPage', {message: "Welcome to the Home Page"});
+
+    if (randomClothe != (clothesData.length-1)) {
+        randomArr.push(clothesData[randomClothe]);
+        randomArr.push(clothesData[randomClothe+1]);
+    } else {
+        randomArr.push(clothesData[randomClothe]);
+        randomArr.push(clothesData[randomClothe-1]);
+    }
+    res.render('shopPage', {message: "Welcome to the Home Page", shopList: randomArr});
 });
 
 app.get('/stickers.html', function(req, res, next) {
     console.log("enter stickers shop");
-    res.render('shopPage', {message: "Stickers and School Supplies"});
+    res.render('shopPage', {message: "Stickers and School Supplies", shopList: stickersData});
 });
 
 app.get('/clothes.html', function(req, res, next) {
@@ -43,6 +65,7 @@ app.get('/clothes.html', function(req, res, next) {
     res.render('shopPage', {message: "Clothes and Accessories", shopList: clothesData});
 });
 
+//post request that checks if a user has data stored in memory aka the json file
 app.post('/login', function(req,res) {
     if(userData[req.body.name]){
         curUser = req.body.name;
